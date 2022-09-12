a289d2df9c52a5b8616ce2aeee9099d690d967d6
Brayden <banjosquash@gmail.com>
2022-09-12 10:44:45 -0400

    installed inquirer, created gitignore, added node module to gitignore, created list of questions withh inquirer

--- index.js
+++ index.js
@@ -1,19 +1,58 @@
-const readmeDataArgs = process.argv.slice(2, process.argv.length);
-console.log(readmeDataArgs);
+const fs = require('fs');
+const inquirer = require('inquirer');
 
-const printReadmeData = readmeDataArr => {
-  for (let i = 0; i < readmeDataArr.length; i++) {
-    console.log(readmeDataArr[i]);
-  }
-
-  console.log('================');
-
-
-
-
-readmeDataArr.forEach((readmeItem) => {
-  console.log(readmeItem)
-});
-};
-
-printReadmeData(readmeDataArgs);
+//questions 
+inquirer
+    .prompt([
+      {
+        type: 'input',
+        message: 'What is your project title?',
+        name: 'title',
+      },
+      {
+        type: 'input',
+        message: 'Enter project description',
+        name: 'description'
+      },
+      {
+        type: 'input',
+        message: 'Enter installation instructions',
+        name: 'installation'
+      },
+      {
+        type: 'input',
+        message: 'Enter instructions for usage',
+        name: 'usage'
+      },
+      {
+        type: 'list',
+        message: 'Choose license',
+        name: 'license',
+        choices: ['MIT License', 'GNU General Public License v3.0', 'Mozilla Public License 2.0']
+      },
+      {
+        type: 'input',
+        message: 'Enter instructions for contributing.',
+        name: 'contributing'
+      },
+      {
+        type: 'input',
+        message: 'Enter test instructions',
+        name: 'testing'
+      },
+      {
+        type: 'input',
+        message: 'Enter questions information',
+        name: 'questions'
+      },
+      {
+        type: 'input',
+        message: 'Enter Github username',
+        name: 'github'
+      },
+      {
+        type: 'input',
+        message: 'Enter e-mail',
+        name: 'email'
+      },
+    ])
